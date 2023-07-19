d27b7d9e9c0a90c9228720ffc2084fa8816441b7
Victor Duque <victorduquerosales@mercadolibre.cl>
2023-07-19 14:13:22 -0400

    ejercio 1

---
+++ teclado/ingresos.go
@@ -0,0 +1,17 @@
+package teclado
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var numeroUno int
+var numeroDOs int
+var leyenda string
+var err error
+
+func IngresoNumeros() {
+	fmt.Println("Ingrese numero uno")
+	scan := bufio.NewScanner(os.Stdin)
+}
